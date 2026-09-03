ea869df3d61661b7666792107261e22625f4b7b4
AHP622 <ahp45592@gmail.com>
2026-09-03 14:23:10 +0000

    target older browsers for compatibility

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "./",
-  build: { outDir: "docs" },
+  build: { outDir: "docs", target: "es2015" },
   server: { host: true, port: 5173 },
 });
